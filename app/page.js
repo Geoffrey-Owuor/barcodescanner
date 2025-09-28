79c7d38d0af7aa83b0d4fdb20755cccbc625a6da
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2025-09-28 08:50:03 +0300

    Third scan test

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import BarCodeScanner from "@/components/BarCodeScanner";
-import CameraView from "@/components/CameraView";
 
 export default function HomePage() {
   return (
     <main>
-      <CameraView />
+      <BarCodeScanner />
     </main>
   );
 }
